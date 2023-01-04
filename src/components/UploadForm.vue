95aa3798c641343fb660b35a136260a9b3c04eae
KumarAmitt <amitt2015@outlook.com>
2023-01-04 19:50:56 +0530

    Add form to upload image

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    UploadForm
+  <div class="dropper">
+    <input type="file">
+    <span>Drag file here!</span>
   </div>
 </template>
 
@@ -11,5 +12,26 @@ export default {
 </script>
 
 <style scoped>
+
+.dropper {
+  height: 30vh;
+  border: 2px dashed #000;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.dropper:hover {
+  background-color: #eee;
+}
+
+input {
+  width: 100%;
+  height: 30vh;
+  position: absolute;
+  opacity: 0;
+}
 
 </style>
